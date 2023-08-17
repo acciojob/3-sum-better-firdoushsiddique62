81ca53cfd66e3bf427fc6ceb23558f955d47d951
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-17 21:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function threeSum(arr, target) {
+ function threeSum(arr, target) {
 // write your code here
+	arr.sort();
+	let diff = 1000000000000;
+
+	for(let i=0;i<arr.length-2;i++){
+		let j = i+1;
+		let k = arr.length-1;
+		while(j<k){
+			let sum = arr[i] + arr[j] + arr[k];
+			if(sum==target)return target;
+			if(sum<target)j++;
+			else k--;
+			if(Math.abs(diff)>Math.abs(target-sum))
+				diff = target-sum;
+		}
+	}
+	return target-diff;
   
 }
 
